d6161a7d5528c4b181df0c2c678c2f7c4892bc1d
cindycallai <cindycallaibraun@yahoo.com.br>
2025-07-29 15:03:47 -0300

    Altera o server

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do AllBooks")
-  console.log("API disponível através da url http://localhost:8000")
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> e758b7713698674f359082bb9d20610def2c1570
+  console.log("API disponível em http://localhost:8000")
 })
